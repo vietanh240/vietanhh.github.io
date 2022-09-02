15c9f8e26280a84a95915fef8b19aeb61e5e092e
wvietanhw <106524611+vietanh240@users.noreply.github.com>
2022-09-02 20:59:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Lại Duy Hưng';
 const giftUrl = 'https://www.facebook.com/photo/?fbid=575209673949904&set=a.199480008189541';
 const eventName = 'Chúc Mừng Sinh Nhật';
 const titleCard = 'Chúc Mừng Sinh Nhật Người Anh Em';
-const contentCard = 'Chúc mừng sinh nhật nhé bạn của tôi!;
+const contentCard = 'Chúc mừng sinh nhật nhé bạn của tôi!';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
