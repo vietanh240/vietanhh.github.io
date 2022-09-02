22042f09bac22f00e61398097ea1b122cf74ba80
wvietanhw <106524611+vietanh240@users.noreply.github.com>
2022-09-02 16:54:25 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Lại Duy Hưng';
+const giftUrl = 'https://www.facebook.com/photo/?fbid=575209673949904&set=a.199480008189541';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Chúc Mừng Sinh Nhật Người Anh Em';
+const contentCard = 'Tao tũng tả tiết túc tái tì. Tày tó tọc tược tông? Tếu tông thì tệ tày. Tếu tó thì tể tao túc tiếp, tinh tật tui tẻ té';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'image.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
