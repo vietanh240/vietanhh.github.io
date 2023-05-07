e7f85911c03006d01f7c9ed95e6e62e97c4f6cfc
wvietanhw <106524611+vietanh240@users.noreply.github.com>
2023-05-07 15:54:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const nameGirl = 'Lại Duy Hưng';
-const giftUrl = 'https://www.facebook.com/photo/?fbid=575209673949904&set=a.199480008189541';
+const nameGirl = 'Nguyễn Tiến Dũng';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100075505382102';
 const eventName = 'Chúc Mừng Sinh Nhật';
-const titleCard = 'Chúc Mừng Sinh Nhật Người Anh Em';
-const contentCard = 'Chúc mừng sinh nhật nhé bạn của tôi! Mỗi người luôn có những thời điểm đánh dấu những phút giây đặc biệt, với tôi ngày bạn ra đời cũng là một thời điểm đặc biệt như vậy đấy ^^ . Chúc bạn sinh nhật nhiều niềm vui! Cầu mong hạnh phúc sẽ làm cuộc đời bạn luôn tràn đầy sự ngọt ngào. ^.^';
+const titleCard = 'Chúc Mừng Sinh Nhật Anh Em';
+const contentCard = 'Tao tũng tả tiết túc tái tì. Tày tó tọc tược tông? Tếu tông thì tệ tày. Tếu tó thì tể tao túc tiếp, tinh tật tui tẻ té';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'image.png';
+const giftImage = 'https://zpsocial-f42-org.zadn.vn/8e8fab13d0393c676528.jpg';
 const base64 = '';
-const giftImageBase64 = "data:image/png;base64, " + base64;
+const giftImageBase64 = "https://zpsocial-f42-org.zadn.vn/8e8fab13d0393c676528.jpg " + base64;
