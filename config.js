90a25e45d08543bfb526aa963fa8902e7a323d10
wvietanhw <106524611+vietanh240@users.noreply.github.com>
2023-05-07 16:25:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 const nameGirl = 'Nguyễn Tiến Dũng';
 const giftUrl = 'https://www.facebook.com/profile.php?id=100075505382102';
 const eventName = 'Chúc Mừng Sinh Nhật';
-const titleCard = 'Chúc Mừng Sinh Nhật Anh Em';
-const contentCard = 'Tao tũng tả tiết túc tái tì. Tày tó tọc tược tông? Tếu tông thì tệ tày. Tếu tó thì tể tao túc tiếp, tinh tật tui tẻ té';
+const titleCard = 'HAPPY BIRTHDAY TIẾN DŨNG';
+const contentCard = ' Nhân dịp sinh nhật anh, em chúc anh luôn mạnh khoẻ, gặp nhiều may mắn trong cuộc sống và thành công trong sự nghiệp của mình ';
 
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
-// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'https://zpsocial-f42-org.zadn.vn/8e8fab13d0393c676528.jpg';
+
+const giftImage = 'Ảnh thẻ.jpg';
 const base64 = '';
-const giftImageBase64 = "https://zpsocial-f42-org.zadn.vn/8e8fab13d0393c676528.jpg " + base64;
+const giftImageBase64 = "Ảnh thẻ.jpg" + base64;
