245eb76420a394707308706b360b8db58c32fc1d
wvietanhw <106524611+vietanh240@users.noreply.github.com>
2023-05-07 16:35:07 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Nguyễn Tiến Dũng';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100075505382102';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'HAPPY BIRTHDAY TIẾN DŨNG';
+const contentCard = ' Nhân dịp sinh nhật anh, em chúc anh luôn mạnh khoẻ, gặp nhiều may mắn trong cuộc sống và thành công trong sự nghiệp của mình ';
+
+
+const giftImage = 'Ảnh thẻ.jpg';
+const base64 = '';
+const giftImageBase64 = "Ảnh thẻ.jpg" + base64;
