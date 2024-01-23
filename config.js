b0cfdd31112b045182b0ac9afd7f672a6a682df3
wvietanhw <106524611+vietanh240@users.noreply.github.com>
2024-01-23 12:46:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Nguyễn Tiến Dũng';
+const nameGirl = 'PHẠM THỊ QUỲNH';
 const giftUrl = 'https://www.facebook.com/profile.php?id=100075505382102';
-const eventName = 'Chúc Mừng Sinh Nhật';
-const titleCard = 'HAPPY BIRTHDAY TIẾN DŨNG';
-const contentCard = ' Nhân dịp sinh nhật anh, em chúc anh luôn mạnh khoẻ, gặp nhiều may mắn trong cuộc sống và thành công trong sự nghiệp của mình ';
+const eventName = 'TẤM THƯ TỎ TÌNH';
+const titleCard = 'GỬI PHẠM THỊ QUỲNH';
+const contentCard = 'Cậu là điều tuyệt vời nhất trong cuộc sống của tớ.Tớ rất hạnh phúc khi được gặp cậu.Làm người yêu tớ nhé?';
 
 
 const giftImage = 'Ảnh thẻ.jpg';
